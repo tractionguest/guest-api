3bc1b21d5b788d0a2564577c9fee0166dbd6aea4
Steven Allen <sallen@tractionguest.com>
2019-06-11 14:21:28 -0700

    Removed submodule guest-dart-sdk

--- build-scripts/dart.sh
+++ build-scripts/dart.sh
@@ -1,3 +1,8 @@
 echo $API_VERSION
 openapi-generator generate -i ./openapi.yml -g dart -o guest-dart-sdk/ -DbrowserClient=false -DpubName=guest_sdk
 cd guest-dart-sdk
+git commit -a -m "Version $API_VERSION"
+git tag $API_VERSION
+git push --tags
+cd ..
+git add guest-dart-sdk

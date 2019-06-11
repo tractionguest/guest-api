aa4f683457d13cdd1091577638741d11f097743c
Steven Allen <sallen@tractionguest.com>
2019-06-11 16:53:20 -0700

    Hopefully skipping build on deploy now

--- build-scripts/dart.sh
+++ build-scripts/dart.sh
@@ -1,2 +1,2 @@
 ./openapi-generator.sh generate -i ./openapi.yml -g dart -o guest-dart-sdk/ -DbrowserClient=false -DpubName=guest_sdk
-git add guest-dart-sdk/*
+git add guest-dart-sdk guest-dart-sdk/.gitignore guest-dart-sdk/.openapi-generator-ignore guest-dart-sdk/.travis.yml guest-dart-sdk/.openapi-generator/VERSION

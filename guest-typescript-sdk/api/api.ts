d46c7de2d9ab485c4b8560fc1a88d688b0fd13ca
Travis CI <builds@travis-ci.com>
2020-02-03 19:53:47 +0000

    [skip travis] Generated SDK version v0.9.31 from TravisCI for build 72

---
+++ guest-typescript-sdk/api/api.ts
@@ -0,0 +1,15 @@
+export * from './emailTemplates.service';
+import { EmailTemplatesService } from './emailTemplates.service';
+export * from './hosts.service';
+import { HostsService } from './hosts.service';
+export * from './invites.service';
+import { InvitesService } from './invites.service';
+export * from './locations.service';
+import { LocationsService } from './locations.service';
+export * from './signins.service';
+import { SigninsService } from './signins.service';
+export * from './users.service';
+import { UsersService } from './users.service';
+export * from './watchlists.service';
+import { WatchlistsService } from './watchlists.service';
+export const APIS = [EmailTemplatesService, HostsService, InvitesService, LocationsService, SigninsService, UsersService, WatchlistsService];

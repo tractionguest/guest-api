a3c8fb26a2550b20e9c9ceb3de503469785b0950
Steven Allen <sallen@tractionguest.com>
2020-04-24 21:26:41 +0000

    Android!

---
+++ build-scripts/android.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./openapi-generator-4.3.sh generate -i ./openapi.yml -g android -o guest-android-sdk/ -c android.json 
+git add guest-android-sdk guest-android-sdk/.gitignore guest-android-sdk/.openapi-generator-ignore guest-android-sdk/.openapi-generator/VERSION

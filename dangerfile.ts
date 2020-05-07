67c89960347cfe8de8ac9ec66d90e7a0df5689b4
Brandon J McKay <bmckay@tractionguest.com>
2020-05-07 13:27:17 -0700

    Add dangerfile and auto_assign GH actions

---
+++ dangerfile.ts
@@ -0,0 +1,42 @@
+/* Constants */
+const branchName = danger.github.pr.head.ref;
+
+/* Steps */
+// Attach Wrike tickets
+const wrikeIds = getUniqueWrikeIds();
+if (wrikeIds.length) {
+  const mappedIds = wrikeIds.map(id => wrikeLink(id)).join(', ');
+  message(`Associated Wrike Tickets:\n${mappedIds}`);
+} else {
+  warn(
+    `You don't seem to have any wrike tickets for this PR!
+    You need to add your wrike ticket to one of your commits, e.g., \`[#1234] Your commit message\``
+  );
+}
+
+// Remind to squash on un-squashed PRs
+if (danger.github.commits.length > 1 && isSinglePr) {
+  message(
+    `Remember to use the \`Squash and Merge\` button to merge!
+    Always remember, your final commit message needs to start with a wrike ticket like, \`[#1234] Your commit message\`.`
+  );
+}
+
+/* Functions */
+function wrikeLink(wrikeId:string) {
+  return `<a href='https://www.wrike.com/open.htm?id=${wrikeId}' target="_blank">#${wrikeId}</a>`;
+}
+
+function getUniqueWrikeIds() {
+  const wrikeIds = danger.github.commits.reduce((acc, obj) => {
+      const msg = obj.commit.message;
+      const wrikeId = msg.match(/^\[\#(\d*)\]/);
+      if (wrikeId) {
+        acc.push(wrikeId[1]);
+      }
+
+      return acc;
+    }, []);
+
+  return [...new Set(wrikeIds)];
+}

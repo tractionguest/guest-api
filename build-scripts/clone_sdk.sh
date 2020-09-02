9756aea0d804f413e2afa21868191b3c4449b682
Steven Allen <sallen@tractionguest.com>
2020-09-02 23:30:59 +0000

    Okay, trying this

--- build-scripts/clone_sdk.sh
+++ build-scripts/clone_sdk.sh
@@ -2,3 +2,13 @@
 
 SDK_NAME=$1
 git clone git@github.com:tractionguest/$SDK_NAME.git
+cd $SDK_NAME
+git ls-remote --exit-code --heads  git@github.com:tractionguest/$SDK_NAME.git $CIRCLE_BRANCH
+if [ "$?" == "1" ] ; then
+  echo "Checking out existing branch $CIRCLE_BRANCH"
+  git checkout $CIRCLE_BRANCH
+  git pull
+else
+  echo "Creating branch $CIRCLE_BRANCH"
+  git checkout -b $CIRCLE_BRANCH
+fi
--- build-scripts/git.sh
+++ build-scripts/git.sh
@@ -4,12 +4,10 @@ SDK_NAME=$1
 
 export GIT_TAG=v$API_VERSION
 cd $SDK_NAME
-git checkout -b $CIRCLE_BRANCH
-git branch --set-upstream-to=origin/$CIRCLE_BRANCH $CIRCLE_BRANCH
-git pull
 git add .
-git commit -m "[ci skip] Generated SDK version v$API_VERSION from TravisCI for build $CIRCLE_BUILD_NUM"
+git commit -m "[ci skip] Generated SDK version v$API_VERSION from CircleCI for build $CIRCLE_BUILD_NUM"
 if [ "$CIRCLE_PULL_REQUESTS" != *"github.com"* -a "$CIRCLE_BRANCH" = "master" ]; then 
   git tag $GIT_TAG -a -m "Generated tag from CircleCI for build $CIRCLE_BUILD_NUM"
 fi
-git push -q --follow-tags git@github.com:tractionguest/guest-api.git HEAD:$CIRCLE_BRANCH
+git push -u origin $CIRCLE_BRANCH
+git push -q --follow-tags

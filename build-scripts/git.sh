9729fed9da82a5b3dbec513901d6d43de4e8e9a3
Steven Allen <sallen@tractionguest.com>
2020-09-02 22:01:57 +0000

    [ci skip] Generated SDK version v from TravisCI for build

--- build-scripts/git.sh
+++ build-scripts/git.sh
@@ -4,7 +4,9 @@ SDK_NAME=$1
 
 export GIT_TAG=v$API_VERSION
 cd $SDK_NAME
-
+git checkout -b $CIRCLE_BRANCH
+git branch --set-upstream-to=origin/$CIRCLE_BRANCH $CIRCLE_BRANCH
+git pull
 git add .
 git commit -m "[ci skip] Generated SDK version v$API_VERSION from TravisCI for build $CIRCLE_BUILD_NUM"
 if [ "$CIRCLE_PULL_REQUESTS" != *"github.com"* -a "$CIRCLE_BRANCH" = "master" ]; then 

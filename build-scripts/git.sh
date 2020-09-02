7197624606cb6a425961ee8c835b66713a5d0d69
Steven Allen <sallen@tractionguest.com>
2020-09-02 21:53:49 +0000

    Committing...

--- build-scripts/git.sh
+++ build-scripts/git.sh
@@ -5,6 +5,7 @@ SDK_NAME=$1
 export GIT_TAG=v$API_VERSION
 cd $SDK_NAME
 
+git add .
 git commit -m "[ci skip] Generated SDK version v$API_VERSION from TravisCI for build $CIRCLE_BUILD_NUM"
 if [ "$CIRCLE_PULL_REQUESTS" != *"github.com"* -a "$CIRCLE_BRANCH" = "master" ]; then 
   git tag $GIT_TAG -a -m "Generated tag from CircleCI for build $CIRCLE_BUILD_NUM"

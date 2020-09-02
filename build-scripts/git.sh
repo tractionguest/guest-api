2bc5261e28f95e41f706173be52406b91f0ed46b
Steven Allen <sallen@tractionguest.com>
2020-09-02 17:19:55 +0000

    Move SDK generation over to CircleCI

--- build-scripts/git.sh
+++ build-scripts/git.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export GIT_TAG=v$API_VERSION
-git commit -m "[skip travis] Generated SDK version v$API_VERSION from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git tag $GIT_TAG -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git push -q --follow-tags git@github.com:tractionguest/guest-api.git HEAD:master
+git commit -m "[ci skip] Generated SDK version v$API_VERSION from TravisCI for build $CIRCLE_BUILD_NUM"
+if [ "$CIRCLE_PULL_REQUESTS" != *"github.com"* -a "$CIRCLE_BRANCH" = "master" ]; then 
+  git tag $GIT_TAG -a -m "Generated tag from CircleCI for build $CIRCLE_BUILD_NUM"
+fi
+git push -q --follow-tags git@github.com:tractionguest/guest-api.git HEAD:$CIRCLE_BRANCH

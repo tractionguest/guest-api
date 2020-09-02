e68185f284d4185de393d296903a8381e35a183d
Steven Allen <sallen@tractionguest.com>
2020-09-02 21:49:39 +0000

    Move SDKs to their own repos

---
+++ build-scripts/clone_sdk.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+SDK_NAME=$1
+git clone git@github.com:tractionguest/$SDK_NAME.git
--- build-scripts/git.sh
+++ build-scripts/git.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+SDK_NAME=$1
+
 export GIT_TAG=v$API_VERSION
+cd $SDK_NAME
+
 git commit -m "[ci skip] Generated SDK version v$API_VERSION from TravisCI for build $CIRCLE_BUILD_NUM"
 if [ "$CIRCLE_PULL_REQUESTS" != *"github.com"* -a "$CIRCLE_BRANCH" = "master" ]; then 
   git tag $GIT_TAG -a -m "Generated tag from CircleCI for build $CIRCLE_BUILD_NUM"

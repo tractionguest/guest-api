d7f5ce5bd1656e03f16564a13aa295acd124ee13
Steven Allen <sallen@tractionguest.com>
2020-09-03 17:06:26 +0000

    Adding debug output

--- build-scripts/clone_sdk.sh
+++ build-scripts/clone_sdk.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 SDK_NAME=$1
+
+printf "${GREEN}Cloning git@github.com:tractionguest/$SDK_NAME.git${NC}\n"
 git clone git@github.com:tractionguest/$SDK_NAME.git
 cd $SDK_NAME
 git ls-remote --exit-code --heads  git@github.com:tractionguest/$SDK_NAME.git $CIRCLE_BRANCH
 if [ "$?" == "1" ] ; then
-  echo "Checking out existing branch $CIRCLE_BRANCH"
+  printf "${GREEN}Checking out EXISTING branch $CIRCLE_BRANCH${NC}\n"
+
   git checkout $CIRCLE_BRANCH
   git pull
 else
-  echo "Creating branch $CIRCLE_BRANCH"
+  printf "${GREEN}Creating branch $CIRCLE_BRANCH${NC}\n"
   git checkout -b $CIRCLE_BRANCH
 fi

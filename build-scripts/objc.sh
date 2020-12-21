e1347fce513d49ab0e6fa0143381987dd3dafe66
Steven Allen <sallen@tractionguest.com>
2020-12-21 20:09:45 +0000

    Single file bundling

--- build-scripts/objc.sh
+++ build-scripts/objc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./openapi-generator-4.3.sh generate -i ./openapi.yml -g objc -o guest-objc-sdk/ -c objc.json 
+./openapi-generator-4.3.sh generate -i ./bundled_openapi.json -g objc -o guest-objc-sdk/ -c objc.json 
 git add guest-objc-sdk guest-objc-sdk/.gitignore guest-objc-sdk/.openapi-generator-ignore guest-objc-sdk/.openapi-generator/VERSION
--- build-scripts/swift.sh
+++ build-scripts/swift.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./openapi-generator-4.3.sh generate -i ./openapi.yml -g swift5 -o guest-swift-sdk/ -c swift5.json 
+./openapi-generator-4.3.sh generate -i ./bundled_openapi.json -g swift5 -o guest-swift-sdk/ -c swift5.json 
 git add guest-swift-sdk guest-swift-sdk/.gitignore guest-swift-sdk/.openapi-generator-ignore guest-swift-sdk/.openapi-generator/VERSION

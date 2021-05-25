a9749df05890b3e2a2707e631d64ed0ee95bbc96
Steven Allen <sallen@tractionguest.com>
2021-05-25 17:57:32 +0000

    Update dart codegen

--- build-scripts/dart.sh
+++ build-scripts/dart.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export API_VERSION=`cat .VERSION`
-./openapi-generator.sh generate -i ./openapi.yml -g dart -o guest-dart-sdk/ -DbrowserClient=false -DpubName=guest_sdk -DpubVersion=$API_VERSION
+./openapi-generator.sh generate -i ./openapi.yml -g dart -o guest-dart-sdk/ -pubName=guest_sdk -pubLibrary=guest_sdk -pubVersion=$API_VERSION
 git add guest-dart-sdk guest-dart-sdk/.gitignore guest-dart-sdk/.openapi-generator-ignore guest-dart-sdk/.travis.yml guest-dart-sdk/.openapi-generator/VERSION

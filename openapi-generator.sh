dd7761ff33bfc0e3a7781ce2dc9d42cbede1f70b
Steven Allen <sallen@tractionguest.com>
2021-05-25 17:27:12 +0000

    Add openapi codegen 5.1

--- openapi-generator.sh
+++ openapi-generator.sh
@@ -4,4 +4,4 @@ java -ea                          \
   -Xms512M                        \
   -Xmx1024M                       \
   -server                         \
-  -jar jars/openapi-generator-cli-4.0.1.jar "$@"
+  -jar jars/openapi-generator-cli-5.1.1.jar "$@"

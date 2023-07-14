064a5f704b0784adae132ac2f740d6f5df819787
Vlad D <vdzhabarov@tractionguest.com>
2023-07-14 11:57:32 -0700

    Add missing ci skip message

--- build-scripts/git.sh
+++ build-scripts/git.sh
@@ -12,8 +12,8 @@ printf "${GREEN}Comitting $SDK_NAME version $GIT_TAG${NC}\n"
 
 cd $SDK_NAME
 git add .
-git commit -m "Generated SDK version v$API_VERSION from CircleCI for build $CIRCLE_BUILD_NUM"
-if [ "$CIRCLE_PULL_REQUESTS" != *"github.com"* -a "$CIRCLE_BRANCH" = "master" ]; then 
+git commit -m "[ci skip] Generated SDK version v$API_VERSION from CircleCI for build $CIRCLE_BUILD_NUM"
+if [ "$CIRCLE_PULL_REQUESTS" != *"github.com"* -a "$CIRCLE_BRANCH" = "master" ]; then
   git tag $GIT_TAG -a -m "Generated tag from CircleCI for build $CIRCLE_BUILD_NUM"
 fi
 git push --set-upstream origin $CIRCLE_BRANCH
